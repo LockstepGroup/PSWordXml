1788261179fa42ac99ca031b3ec7047fc98ac40c
Brian Addicks <brian@addicks.us>
2019-04-11 07:14:44 -0400

    bold/italic handling

--- PSWordXml/Public/Resolve-WordText.ps1
+++ PSWordXml/Public/Resolve-WordText.ps1
@@ -7,7 +7,49 @@ function Resolve-WordText {
 
     $VerbosePrefix = "Resolve-WordText:"
 
+    $BoldOutput = @()
 
+    function IsEven($i) {
+        if (($i % 2) -eq 0) {
+            $true
+        } else {
+            $false
+        }
+    }
 
-    $Output
+    # process bold text
+    $BoldSplit = $Text.Split('**')
+    $i = 0
+    foreach ($run in $BoldSplit) {
+        $i++
+        $new = "" | Select-Object -Property Text, Bold, Italic
+        $new.Text = $run
+        $new.Bold = $false
+        $new.Italic = $false
+        if (IsEven $i) {
+            $new.Bold = $true
+        }
+        $BoldOutput += $new
+    }
+
+    # process italic text
+    $ItalicOutput = @()
+    foreach ($boldrun in $BoldOutput) {
+        $ItalicSplit = $boldrun.Text.Split('_')
+        $i = 0
+        foreach ($run in $ItalicSplit) {
+            $i++
+            if ($run -eq "") { continue }
+            $new = "" | Select-Object -Property Text, Bold, Italic
+            $new.Text = $run
+            $new.Bold = $boldrun.Bold
+            $new.Italic = $false
+            if (IsEven $i) {
+                $new.Italic = $true
+            }
+            $ItalicOutput += $new
+        }
+    }
+
+    $ItalicOutput
 }
